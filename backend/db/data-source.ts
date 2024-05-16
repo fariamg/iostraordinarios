d38fb9a05ccf2faa433065023d9b85138e69262a
davi <daviferreiradev@gmail.com>
2024-05-15 22:37:51 -0300

    fix: data-source

--- backend/db/data-source.ts
+++ backend/db/data-source.ts
@@ -1,13 +1,9 @@
-import dataSourceProd, { dataSourceOptions as dataSourceOptionsProd } from "./data-source-prod";
-import dataSourceLocal, { dataSourceOptions as dataSourceOptionsLocal } from "./data-source-local";
+import DataSourceProd from "./data-source-prod";
+import DataSourceLocal from "./data-source-local";
 import env from "config/env";
 
 console.log(`Using ${env().node_env} configuration`);
 
-export const dataSourceOptions = env().node_env === "production"
-    ? dataSourceOptionsProd
-    : dataSourceOptionsLocal;
-
 export default env().node_env === "production"
-    ? dataSourceProd
-    : dataSourceLocal;
+    ? DataSourceProd
+    : DataSourceLocal;
