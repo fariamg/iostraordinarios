f1053c815eb13ed14a7bb3fb26886e09a7cda97d
davi <daviferreiradev@gmail.com>
2024-05-15 22:59:14 -0300

    fix: Ensure @nestjs/cli is available during build using npx

--- backend/db/data-source.ts
+++ backend/db/data-source.ts
@@ -1,8 +1,12 @@
-import DataSourceProd from "./data-source-prod";
-import DataSourceLocal from "./data-source-local";
+import DataSourceProd, { dataSourceOptions as dataSourceOptionsProd } from "./data-source-prod";
+import DataSourceLocal, { dataSourceOptions as dataSourceOptionsLocal } from "./data-source-local";
 import env from "config/env";
 
 console.log(`Using ${env().node_env} configuration`);
+
+export const dataSourceOptions = env().node_env === "production"
+    ? dataSourceOptionsProd
+    : dataSourceOptionsLocal;
 
 export default env().node_env === "production"
     ? DataSourceProd
