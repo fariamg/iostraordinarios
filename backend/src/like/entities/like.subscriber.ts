c459375d51e8b3e8dd1164fc08ccdc5b6de7ab14
Gabriel <fariamg4@gmail.com>
2024-05-20 13:23:51 -0300

    feat: add like count decrease

--- backend/src/like/entities/like.subscriber.ts
+++ backend/src/like/entities/like.subscriber.ts
@@ -1,4 +1,4 @@
-import { EventSubscriber, EntitySubscriberInterface, InsertEvent, Repository} from 'typeorm'
+import { EventSubscriber, EntitySubscriberInterface, InsertEvent, Repository, RemoveEvent} from 'typeorm'
 import { Like } from './like.entity';
 import { Publish } from 'src/publish/entities/publish.entity';
 
@@ -10,6 +10,16 @@ export class LikeSubscriber implements EntitySubscriberInterface<Like> {
     }
     
     async afterInsert(event: InsertEvent<Like>) {
+        const publishRep: Repository<Publish> = event.connection.manager.getRepository<Publish>('publishes');
+        const likeRep: Repository<Like> = event.connection.manager.getRepository<Like>('likes');
+
+        likeRep.count({ where: { publish: { id: event.entity.publish.id }}})
+        .then((count: number) => {
+            publishRep.update({id: event.entity.publish.id}, {likesCount: count})
+        });
+    }
+
+    async afterRemove(event: RemoveEvent<Like>) {
         const publishRep: Repository<Publish> = event.connection.manager.getRepository<Publish>('publishes');
         const likeRep: Repository<Like> = event.connection.manager.getRepository<Like>('likes');
 
